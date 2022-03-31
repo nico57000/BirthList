28638c0768cf462c1b517dd0032b0c75f14811ae
nico57000 <falcettanicolaspgmail.com>
2022-03-31 17:46:19 +0200

    Get and Update

---
+++ BirthList/BirthList/Controllers/BirthItemController.cs
@@ -0,0 +1,75 @@
+﻿using BirthList.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace BirthList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BirthItemController : ControllerBase
+    {
+
+        // GET: api/<BirthItemController>
+        [HttpGet]
+        public async Task<IEnumerable<BirthItem>> GetAsync()
+        {
+
+            List<BirthItem> birthItems = new List<BirthItem>();
+            var directory = Directory.GetCurrentDirectory();
+            var filePath = Path.Combine(directory, "data.json");
+
+            using (var streamReader = new StreamReader(filePath))
+            {
+                var jsonreader = await streamReader.ReadToEndAsync();
+
+                birthItems = JsonSerializer.Deserialize<List<BirthItem>>(jsonreader);
+            }
+            
+
+            return birthItems;
+        }
+
+        // PUT api/<BirthItemController>/5
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody] BirthItem birthItem)
+        {
+            List<BirthItem> birthItems = new List<BirthItem>();
+            var directory = Directory.GetCurrentDirectory();
+            var filePath = Path.Combine(directory, "data.json");
+
+            using (var streamReader = new StreamReader(filePath))
+            {
+                var jsonreader = await streamReader.ReadToEndAsync();
+
+                birthItems = System.Text.Json.JsonSerializer.Deserialize<List<BirthItem>>(jsonreader);
+            }
+
+
+            var toupdate = birthItems.Find(x=> x.Id == id);
+            toupdate.Name = birthItem.Name;
+            toupdate.isAvaillable = birthItem.isAvaillable;
+
+
+            /// Modifier le fichier
+            for (int i = 0; i < birthItems.Count; i++)
+            {
+                if (birthItems[i].Id == toupdate.Id)
+                {
+                    birthItems[i].isAvaillable = toupdate.isAvaillable;
+                }
+            }
+
+
+
+            /// Réécrire le fichier
+            string jsonString = JsonSerializer.Serialize(birthItems);
+            System.IO.File.WriteAllText(filePath, jsonString);
+
+        }
+
+    }
+}
---
+++ BirthList/BirthList/Models/BirthItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BirthList.Models
+{
+    public class BirthItem
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LinkTo{ get; set; }
+        public string FilePath { get; set; }
+        public bool isAvaillable { get; set; }
+    }
+}
